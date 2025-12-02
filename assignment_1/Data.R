14d2ae48d7b804bbfa99a0853e7331d9b05f969d
LeonSavi <leonardo.saviane@gmail.com>
2025-12-02 15:39:20 +0100

    first commit

--- assignment_1/Data.R
+++ assignment_1/Data.R
@@ -6,36 +6,52 @@ set.seed(1)
 
 N <- 10000
 
+
+# E
+# changes in Economics Outlook/Market Sentiment
+# Our unobservable variable (they ask us for a Open Door Path)
+# my idea is that it can affect Demand And Supply, positive outlook increase in overall Supply and demand
+
+eco_outlook <- rnorm(N,0,100)
+
+
 # I
 # AI Investment: change in cost of investment for short term funds, 
 # i.e. change in interest rates (the lower the better) -> if they go up it is more expensive to get funds
+# can assume that the central bank wants to control short term inflation and does not care about long term 
+# -> therefore inv_cost is referred only to short term funding
 inv_cost <- rnorm(N, 0, 2)
 # C
 # change in raw materials supply: how the production/extraction of commodities change,
 # shock/error can be reffered as when a new mine is found or somthing or some disruption in production
+# not effected by economic outlook,
 commodities <- rnorm(N,0,75)
 # S
 # changes in supply/Market Capacity of production:
 # we can think that for a change in raw material the supply is halved/1.5
-# Also the capacity/supply can increase or decrease depending on the interest rate fluctuations, 
-supply <- 1.5*commodities + (-20)*inv_cost + rnorm(N,0,20)
+# Also the capacity/supply can increase or decrease depending on the interest rate fluctuations,
+# positive changes in economic outlook make them increase the supply
+supply <- 1.5*commodities + (-20)*inv_cost + 0.5*eco_outlook + rnorm(N,0,20)
 # L
 # Logistics/Distribution/Inventory changes/prices
 # we can think it as the number of trucks needed to meet the shock in supply
 # we can also think that interest rates do not have a big impact
 # basically logistics companies change prices/costs according to supply
-logistics <- 0.7*supply + (-10)*inv_cost + rnorm(N,0,25) # maybe we can delete this?
+# rigid to E, deeply depends on supply
+logistics <- 0.7*supply + (-10)*inv_cost + 0.1*eco_outlook  + rnorm(N,0,25) # maybe we can delete this?
 # D
 # Aggregate demand AI change, 
 # for a rule we can say that the demand is ~twice how much is readily available
 # according to the availability(logisitcs) demand is increase
 # it is deeply interconnected with the investment cost and volatile (depending on market fluctuations)
-demand <- 0.9*logistics + (-35)*inv_cost + rnorm(N,0,60)
+# Demand is strongly driven by future perspective (E)
+demand <- 0.9*logistics + (-35)*inv_cost  + 0.6*eco_outlook + rnorm(N,0,30)
 # P
-# change in market price of RAM: 
+# change in current market price of RAM: 
 # we can assume simply that RAM prices are mainly demand driven, but also the supply have an impact
 # demand shock increase the price
 # supply shock decrease
+# I dont know if we should add E?
 prices <- 0.7*demand + (-0.3)*supply + rnorm(N,0,15)
 
 # df
@@ -89,6 +105,9 @@ dag {
   L -> D
   S -> P
   D -> P
+  E -> D
+  E -> S
+  E -> L
 
 }
 ',)
